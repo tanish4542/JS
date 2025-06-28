e8afb5a8c4e83e38f9873701e97d68473ee6a286
Tanish Arora <tanisharora4542@gmail.com>
2025-06-28 09:07:17 +0000

    old i dont remeber

---
+++ 7_4_PROJECT/backend.js
@@ -0,0 +1,32 @@
+const randomnumber=parseInt(Math.random()*100+1)
+
+const submit=document.querySelector('#subt')
+const userinput=document.querySelector('#guessField')
+const guess=document.querySelector('.guesses')
+const remaining=document.querySelector('.lastResult')
+const loworhigh=document.querySelector('.lowOrHigh')
+const startover=document.querySelector('.resultParas')
+
+const p=document.createElement('p')
+
+let prevguess=[]
+let nog=1;
+
+let playgame=true
+
+if(playgame)
+{
+    submit.addEventListener('click',)
+}
+function validate_guess(guess) {
+    
+}
+function check_guess(guess) {
+    
+}
+function display_message(message) {
+    
+}
+function display_guess(guess) {
+    
+}
